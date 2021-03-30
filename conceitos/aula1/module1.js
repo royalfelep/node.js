1720febeb2cb123ee4b3294c0b88e9a401947647
Felipe da Cruz <bsb.felipecorrea@gmail.com>
2021-03-30 00:32:52 -0300

    Aulas node.js

---
+++ conceitos/aula1/module1.js
@@ -0,0 +1,8 @@
+this.oi = "Olá, tudo bem?"
+exports.bomDia = "Bom dia!!"
+module.exports.saudacoes = { // preferência
+    bomDia: "Bom dia!!",
+    boaTarde(){
+        console.log("Boa tarde!")
+    }
+}
---
+++ conceitos/aula1/modulesClient.js
@@ -0,0 +1,4 @@
+const module1 = require('./module1')
+
+module1.saudacoes.boaTarde()
+console.log(module1.oi)
