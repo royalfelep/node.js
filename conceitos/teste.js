c7c713aac537212286cfc580b455ba9a00f432c5
Felipe da Cruz <bsb.felipecorrea@gmail.com>
2021-03-31 12:35:28 -0300

    aulas node.js

---
+++ conceitos/teste.js
@@ -0,0 +1,10 @@
+const premier={
+                porte: "pequeno",
+                idade: "adulto",
+                peso:{
+                    1: "R$33,90",
+                    2.5: "R$71.90"
+                } 
+            }
+
+console.log(premier.peso["2.5"])
