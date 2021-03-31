c479c5af8fce24a3307f4278340bcd6ee89453f6
Felipe da Cruz <bsb.felipecorrea@gmail.com>
2021-03-31 15:30:36 -0300

    organizando

--- conceitos/aula1/module1.js
+++
@@ -1,8 +0,0 @@
-this.oi = "Olá, tudo bem?"
-exports.bomDia = "Bom dia!!"
-module.exports.saudacoes = { // preferência
-    bomDia: "Bom dia!!",
-    boaTarde(){
-        console.log("Boa tarde!")
-    }
-}
--- conceitos/aula1/modulesClient.js
+++
@@ -1,4 +0,0 @@
-const module1 = require('./module1')
-
-module1.saudacoes.boaTarde()
-console.log(module1.oi)
