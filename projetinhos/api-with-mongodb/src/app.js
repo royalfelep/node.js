962720982c8514526dcbb9d72d10299314269d4b
Felipe da Cruz <bsb.felipecorrea@gmail.com>
2021-04-07 16:09:43 -0300

    Adicionando cadastroRoute

--- projetinhos/api-with-mongodb/src/app.js
+++ projetinhos/api-with-mongodb/src/app.js
@@ -1,13 +1,15 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const app = express()
-//'----------------------------------------------'
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+const cadastroRoute = require('./routes/cadastro')
+
 //'----------------------------------------------'
 
-app.get('/', (req, res) => {
-    res.send('OK')
-})
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+//'----------------------------------------------'
+
+app.use('/cadastro', cadastroRoute)
 
 app.listen(process.env.PORT || 3000)
