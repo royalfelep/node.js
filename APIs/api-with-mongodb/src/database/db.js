dd5ade3212ab20068cd3263724da49e329eb0b06
Felipe da Cruz <bsb.felipecorrea@gmail.com>
2021-04-07 22:44:00 -0300

    função para inicializar conexão ao mongoDB

--- APIs/api-with-mongodb/src/database/db.js
+++ APIs/api-with-mongodb/src/database/db.js
@@ -1,8 +1,12 @@
 const mongoose = require('mongoose')
 require('dotenv/config')
 
-mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true, useUnifiedTopology: true }, () => {
-    console.log('Connected to MongoDB Cloud')
-})
+function db_init() {
+    mongoose.connect(
+        process.env.DB_CONNECT,
+        { useNewUrlParser: true, useUnifiedTopology: true },
+        () => console.log('Connected to MongoDB Atlas')
+    )
+}
 
-module.exports = mongoose
+module.exports = db_init
