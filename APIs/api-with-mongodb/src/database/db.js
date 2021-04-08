73f5d60ba31ea3f860d134b13414c6b675adda67
Felipe da Cruz <bsb.felipecorrea@gmail.com>
2021-04-07 23:49:47 -0300

    Atualizando função de conexão ao MongoDB

--- APIs/api-with-mongodb/src/database/db.js
+++ APIs/api-with-mongodb/src/database/db.js
@@ -2,11 +2,14 @@ const mongoose = require('mongoose')
 require('dotenv/config')
 
 function db_init() {
-    mongoose.connect(
-        process.env.DB_CONNECT,
-        { useNewUrlParser: true, useUnifiedTopology: true },
-        () => console.log('Connected to MongoDB Atlas')
-    )
+    mongoose
+        .connect(process.env.DB_CONNECT, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true,
+        })
+        .then(() => console.log("Database connected to MongoDB Atlas"))
+        .catch(err => console.log(err));
 }
 
 module.exports = db_init
